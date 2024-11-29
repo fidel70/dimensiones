d08fd7855b1b2ce71f1ee823829959e0a7d2fbde
fidel70 <49075849+fidel70@users.noreply.github.com>
2024-11-29 10:29:59 -0500

    Update RegistroDimensiones.tsx

--- src/app/pacientes/P001/RegistroDimensiones.tsx
+++ src/app/pacientes/P001/RegistroDimensiones.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+/*import { useState, useEffect } from 'react'*/
 import { format } from 'date-fns'
 import { ChevronUp, ChevronDown, Calendar, Activity, Brain, Users } from 'lucide-react'
 import {
